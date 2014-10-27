fe7e4eace8892f50be5073aafbdf556905671e79
pat(nan.yu)2014 <nan.yu.pat@gmail.com>
2014-10-26 23:51:47 -0700

    add get ip method in socket java class

--- src/project1/cmpe275/sjsu/model/Socket.java
+++ src/project1/cmpe275/sjsu/model/Socket.java
@@ -1,4 +1,6 @@
 package project1.cmpe275.sjsu.model;
+import java.net.*;
+import java.util.*;
 
 public class Socket {
 	private String ip=null;
@@ -28,5 +30,40 @@ public class Socket {
 	public void setPort(int port) {
 		this.port = port;
 	}
+//get local machine ip address
+public String getLocalIP() {
+       String ip = "";
+       try {
+           Enumeration<?> e1 = (Enumeration<?>) NetworkInterface.getNetworkInterfaces();
+           while (e1.hasMoreElements()) {
+               NetworkInterface ni = (NetworkInterface) e1.nextElement();
+               if (!ni.getName().equals("eth0")) {
+                   continue;
+               } else {
+                   Enumeration<?> e2 = ni.getInetAddresses();
+                   while (e2.hasMoreElements()) {
+                       InetAddress ia = (InetAddress) e2.nextElement();
+                       if (ia instanceof Inet6Address)
+                           continue;
+                       ip = ia.getHostAddress();
+                   }
+                   break;
+               }
+           }
+       } catch (SocketException e) {
+           e.printStackTrace();
+           System.exit(-1);
+       }
+       return ip;
+   }
+
+private static String byteHEX(byte ib) {
+       char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a','b', 'c', 'd', 'e', 'f' };
+       char[] ob = new char[2];
+       ob[0] = Digit[(ib >>> 4) & 0X0F];
+       ob[1] = Digit[ib & 0X0F];
+       String s = new String(ob);
+       return s;
+   }
 	
 }
